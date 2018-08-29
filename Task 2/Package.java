0363f9ecabe64947df976dcc9a64373b6927db56
applecrumble123 <30610249+applecrumble123@users.noreply.github.com>
2018-08-29 15:33:09 +0800

    task 2

---
+++ Task 2/Package.java
@@ -0,0 +1,22 @@
+public class Package
+{
+    private int weight;
+    private double shippingCost;
+    private char shippingMethod;
+
+    final char AIR = 'A';
+    final char TRUCK = 'T';
+    final int LOWWT = 9;
+    final double LOWAIR = 2.00;
+    final double LOWTRUCK = 1.50;
+    final double LOWMAIL = 0.50;
+    final int MEDWT = 17;
+    final double MEDAIR = 3.00;
+    final double MEDTRUCK = 2.35;
+    final double MEDMAIL = 1.50;
+    final double HIGHAIR = 4.50;
+    final double HIGHTRUCK = 3.25;
+    final double HIGHMAIL = 2.15;
+
+
+}
